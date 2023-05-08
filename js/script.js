c74346e9ccbd32fb91f2a4f5286d3f60d0810cf9
Fedor <fedkon96@gmail.com>
2023-05-08 12:18:05 +0300

    start prodject

---
+++ js/script.js
@@ -0,0 +1,94 @@
+"use sctrict";
+
+// console.log('Hello World') 
+// console.log("JS")
+
+// var obj = {
+//     name: "Rezo",
+//     age: 26,
+// };
+
+// console.log(obj.age)
+
+// var img = ["name", "age", 26, 1996];
+// console.log(img[0])
+
+// let testObj = [15, 18, 130, '8 550 00', 45];
+// console.log(testObj[3]);
+
+// var alia = {
+//     '1': "konopelko",
+//     age: 27
+// };
+
+// console.log(alia[1]);
+
+// alia["wymen"] = "super";
+// console.log(alia["wymen"]);
+
+// var wymen = 'wymen';
+// alia[wymen] = '1234';
+
+// console.log(alia["wymen"])
+
+// alert('helloy');
+
+// const otvet = confirm("Are you here");
+// console.log(otvet);
+
+// const ansver = +prompt('Как тебя зовут?', 'Mort');
+// console.log(ansver + 5);
+
+// const answers = [];
+// answers[0] = prompt('Как тебя зовут?', '');
+// answers[1] = prompt('Сколько тебе лет?', '');
+// answers[2] = prompt('Месяц рождения?', '');
+
+// document.write(answers);
+
+// let user = "Rezo";
+// console.log(`Hello ${user}`);
+
+// let name = prompt('Как тебя зовут?', "");
+// console.log(`Привет ${name}`);
+
+// let increment = 10,
+//     decrement = 10;
+
+// increment = increment + 3;
+
+// console.log(++increment);
+// console.log(--decrement);
+
+// -------------
+// let category = 'uk';
+// console.log(`https://wol.jw.org/${category}`);
+// -------------
+
+// let names = 'Rezo';
+// let name = prompt("Как тебя зовут?", '');
+// console.log(`${name} - Hi`);
+
+//  ОПЕРАТОРЫ
+const name = "Rezo";
+const hi = " helloy";
+console.log(name + hi);
+console.log(5 + +"5");
+
+let number = "5";
+let number1 = 6;
+
+number++;
+
+console.log(--number);
+
+console.log(137 % 3);
+
+const windoy = false;
+const dor = false;
+
+console.log( windoy && !dor);
+
+
+var kalkulator = 1 + 1 * 2 !== "3";
+console.log(kalkulator);
